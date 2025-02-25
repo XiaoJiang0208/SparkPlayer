059e695e001ae46ea47bd40fcf292a0c92aae556
XiaoJiang0208 <jjm1837080396@gmail.com>
2025-02-25 22:52:44 +0800

    添加头文件 include.h，包含 D 系列和 Q 系列的必要组件

---
+++ include.h
@@ -0,0 +1,31 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+// D 系列头文件
+#include <DArrowButton>
+#include <DButtonBox>
+#include <DFontSizeManager>
+#include <DLabel>
+#include <DMainWindow>
+#include <DPushButton>
+#include <DScrollArea>
+#include <DSlider>
+#include <DStyle>
+#include <DTitlebar>
+#include <DWidget>
+
+// Q 系列头文件
+#include <QButtonGroup>
+#include <QHBoxLayout>
+#include <QImage>
+#include <QImageReader>
+#include <QList>
+#include <QLayout>
+#include <QPainter>
+#include <QPainterPath>
+#include <QPixmap>
+#include <QResizeEvent>
+#include <QString>
+#include <QUrl>
+
+#endif // INCLUDE_H
